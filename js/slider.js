caf4d6281bb0bda4258a7c009ab23fc3b2fd7568
A1exSereb <72438956+A1exSereb@users.noreply.github.com>
2021-04-22 13:57:34 +0600

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,89 +1,134 @@
-const prev = document.querySelector("#btn-prev"),
-      next = document.querySelector("#btn-next"),
-      slides = document.querySelectorAll('.slide'),
-      dots = document.querySelectorAll('.dot');
+const slides = Array.from(document.querySelectorAll(".slide")),
+slider = document.querySelectorAll(".slider"),
+nextBtn = document.querySelector('.btn-next'),
+prevBtn = document.querySelector('.btn-prev');
 
-let index = 0
-
-function unfade(element) {
-  let op = 0.1;  
-  element.style.opacity = 0;
-  next.classList.remove('active-btn');
-  prev.classList.remove('active-btn');
-  let timer = setInterval( () => {
-      element.style.opacity = op;
-      op += 0.1;
-      if (element.style.opacity == 1){
-        clearInterval(timer);
-        addActive();
+function getNextPrev() {
+  const activeSlide = document.querySelector(".slide.active");
+  const activeIndex = slides.indexOf(activeSlide);
+  let next, prev;
+  if (activeIndex === slides.length - 1) {
+    next = slides[0];
+  } else {
+    next = slides[activeIndex + 1];
+  }
+  if (activeIndex === 0) {
+    prev = slides[slides.length - 1];
+  } else {
+    prev = slides[activeIndex - 1];
+  }
+  return [next, prev];
+}
+function getPosition() {
+  const activeSlide = document.querySelector(".slide.active");
+  const activeIndex = slides.indexOf(activeSlide);
+  const [next, prev] = getNextPrev();
+  slides.forEach((slide, index) => {
+    if (index === activeIndex) {
+      slide.style.transform = "translateX(0)";
+    } else if (slide === prev) {
+      slide.style.transform = "translateX(-100%)";
+    } else if (slide === next) {
+      slide.style.transform = "translateX(100%)";
+    } else {
+      slide.style.transform = "translateX(100%)";
     }
-  }, 100);
+      slide.classList.remove("top");
+  });
+}
+
+function getNextSlide() {
+  animation('next');
+}
+function getPrevSlide() {
+  animation('prev');
+}
+function animation(getDirection){
+  const current = document.querySelector(".active");
+  const [next, prev] = getNextPrev();
+  removeEvents();
+  if(getDirection === 'next'){
+    current.classList.add("top");
+    next.classList.add("top");
+
+  let nextTraslate = 100;
+  let translate = 0;
+
+  let timer = setInterval(() => {
+  current.style.transform = `translate(${translate -= 1}%)`;
+  next.style.transform = `translateX(${nextTraslate -= 1}%)`;
   
+  if(translate == -100 && nextTraslate == 0){
+    current.classList.remove("active");
+    next.classList.add("active");
+    getPosition();
+    getActiveDot();
+    addEvents();
+    clearInterval(timer);
+  }
+  }, 5);
+  }
+  if(getDirection === 'prev'){
+    current.classList.add("top");
+    prev.classList.add("top");
+    
+
+    let prevTraslate = -100;
+    let translate = 0;
+  
+    let timer = setInterval(() => {
+      current.style.transform = `translate(${translate += 1}%)`;
+      prev.style.transform = `translateX(${prevTraslate += 1}%)`;
+      
+      if(translate == 100 && prevTraslate == 0){
+        current.classList.remove("active");
+        prev.classList.add("active");
+        getPosition();
+        getActiveDot();
+        addEvents();
+        clearInterval(timer);
+      }
+      }, 5);
+  }
 }
+getPosition();
 
-const activeSlide = n =>{
-  for(slide of slides){
-    slide.classList.remove('active');
-  }
-  slides[n].classList.add('active');
-  unfade(slides[n]);
-};
-const activeDot = n =>{
-  for(dot of dots){
-    dot.classList.remove('active');
-  }
-  dots[n].classList.add('active');
-};
 
-const currentSlide = index =>{
-  activeSlide(index);
-  activeDot(index);
+function getActiveDot() {
+  const allDots = document.querySelectorAll(".dots .dot");
+  allDots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  const activeSlide = document.querySelector(".slide.active");
+  const activeIndex = slides.indexOf(activeSlide);
+  allDots[activeIndex].classList.add("active");
 }
-
-const nextSlide = () =>{
-  if(index == slides.length -1){
-    index = 0;
-    currentSlide(index);
-  }else{
-    index++;
-    currentSlide(index);
-  }
-};
-
-const prevSlide = () =>{
-  if(index == 0){
-    index = slides.length - 1;
-    currentSlide(index);
-  }else{
-    index--;
-    currentSlide(index);
-  }
-};
-
-dots.forEach((dot, ind) =>{
- dot.addEventListener('click',() =>{
-  if(index != ind){
-    currentSlide(ind);
-  index = ind;
-  }
- });
-});
-
-next.addEventListener('click',() =>{
-  if(next.classList.contains('active-btn')){
-    nextSlide();
-  }
-});
-prev.addEventListener('click',() =>{
-  if(next.classList.contains('active-btn')){
-    prevSlide();
-  }
-});
-
-const addActive = () =>{
-  next.classList.add('active-btn');
-  prev.classList.add('active-btn');
+function functionalDots() {
+  const allDots = document.querySelectorAll(".dots .dot");
+  allDots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      getDotSlide(index);
+    });
+  });
 }
+function getDotSlide(index) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  const current = slides[index];
+  current.classList.add("active");
+  getPosition();
+  getActiveDot();
+}
+getActiveDot();
+functionalDots();
 
-
-
+function addEvents(){
+  nextBtn.addEventListener('click', getNextSlide);
+  prevBtn.addEventListener('click', getPrevSlide);
+}
+function removeEvents(){
+  nextBtn.removeEventListener('click', getNextSlide);
+  prevBtn.removeEventListener('click', getPrevSlide);
+}
+addEvents();
